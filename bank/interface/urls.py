69023d0ddef397e43073dfe8558bcde0485b0479
Douglas Jesus <dougecomp@gmail.com>
2024-05-25 13:15:44 -0300

    :sparkles: feat: create a sing up page

--- bank/interface/urls.py
+++ bank/interface/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, login_page
+from .views import home_page, sign_in_page, sign_up_page
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('login/', login_page, name='login_page'),
+    path('signin/', sign_in_page, name='sign_in_page'),
+    path('signup/', sign_up_page, name='sign_up_page'),
 ]
--- bank/interface/views.py
+++ bank/interface/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .forms import FormClient
 
 # Create your views here.
 def home_page(request):
     context = {}
     return render(request, 'home_page.html', context=context)
 
-def login_page(request):
+def sign_in_page(request):
     context = {}
-    return render(request, 'login_page.html', context=context)
+    return render(request, 'sign_in_page.html', context=context)
+
+def sign_up_page(request):
+    context = {}
+    return render(request, 'sign_up_page.html', context=context)
