e130aeaad91f1620cc5cea554b81fc8a40f0fb50
Douglas Jesus <dougecomp@gmail.com>
2024-05-25 12:40:23 -0300

    :sparkles: create interbank transactions views

--- bank/interbank/views.py
+++ bank/interbank/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def transfer(request, bank):
+    # Implementar view para solicitar requisição a outro banco.
+    pass
+
+# Create your views here.
+def receive(request, bank):
+    if request.method == "POST":
+        ...
+    # Implementar view para receber requisição de outro(s) banco(s).
+    ...
