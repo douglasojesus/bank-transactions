7c59e917f949b50a1c0b53ff056bbc8dbbfe8de9
Douglas Jesus <dougecomp@gmail.com>
2024-05-25 12:39:08 -0300

    :sparkles: create client model

--- bank/accounts/models.py
+++ bank/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser # Importar AbstractUser
 
-# Create your models here.
+class Client(models.Model, AbstractUser):
+    # Fields in User: password, username, first_name, last_name, email.
+    _balance = models.DecimalField()
+
+    def __str__(self):
+        return self.first_name + self.last_name
+    
+    def get_saldo(self):
+        return self._balance
+
+    def receive_transaction(self, value):
+        self._balance += value
+        return True
+    
