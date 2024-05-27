eae23f31a9192a580f2691960407a563b4c0700d
Douglas Jesus <dougecomp@gmail.com>
2024-05-27 13:22:42 -0300

    :construction: transactions inter banks

--- bank/transactions/views.py
+++ bank/transactions/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from accounts. models import Client # This bank client
+from django.http import HttpResponse
+import requests
 
 # Arquivo responsável por lidar com a comunicação entre bancos. Requisições recebidas de outros bancos e requisições a serem feitas para outros bancos.
 
 # Interface makes request
 # is_authenticated?
-def transfer(request):
+def transfer(request, value_to_transfer, bank_to_transfer, client_to_transfer):
     # Implementar view para solicitar requisição a outro banco.
+    bank_client = Client.objects.filter(username=request.user) # Catch this bank client
+    if bank_client.balance < value_to_transfer:
+        pass # return error
+    # make request to another bank
+
+
     pass
 
 # Other bank makes request
