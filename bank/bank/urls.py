bb88cf22492ce2fa883c25c5ba456657e3b28958
Douglas Jesus <dougecomp@gmail.com>
2024-05-28 23:00:23 -0300

    :lipstick: style: create a home page style

--- bank/bank/urls.py
+++ bank/bank/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from interface.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page, name='home_page'),
     path('interface/', include('interface.urls')),
     path('auth/', include('auth_service.urls')),
     path('transaction/', include('transactions.urls')),
