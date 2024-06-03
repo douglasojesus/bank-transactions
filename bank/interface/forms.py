c2a7734d0249eee8c0ba811d57ea84b13dc6b889
Douglas Jesus <dougecomp@gmail.com>
2024-06-03 13:25:19 -0300

    :sparkles: feat: create a deposit feature

--- bank/interface/forms.py
+++ bank/interface/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 class TransactionForm(forms.Form):
     value_to_transfer = forms.DecimalField(max_digits=10, decimal_places=2)
-    bank_to_transfer = forms.CharField(max_length=100)
-    client_to_transfer = forms.CharField(max_length=100)
+    port_to_transfer = forms.CharField(max_length=36, required=False)
+    ip_to_transfer = forms.CharField(max_length=6, required=False)
+    client_to_transfer = forms.CharField(max_length=100, required=False)
 
     
--- bank/interface/views.py
+++ bank/interface/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .forms import TransactionForm
 from transactions.views import transfer
 from accounts.models import Client
+from django.contrib import messages
 
 # Create your views here.
 def home_page(request):
@@ -25,19 +26,26 @@ def transaction_page(request):
         if request.method == 'POST':
             form = TransactionForm(request.POST)
             if form.is_valid(): 
-
                 action = request.POST.get('choice')
-
                 if action == 'transfer':
                     value_to_transfer = form.cleaned_data['value_to_transfer']
-                    bank_to_transfer = form.cleaned_data['bank_to_transfer']
+                    ip_to_transfer = form.cleaned_data['ip_to_transfer']
+                    port_to_transfer = form.cleaned_data['port_to_transfer']
                     client_to_transfer = form.cleaned_data['client_to_transfer']
-                    # Redireciona para a função de transferência existente em transactions
-                    response = transfer(request, value_to_transfer, bank_to_transfer, client_to_transfer)
-                    return response
-                
+                    if ip_to_transfer and port_to_transfer and client_to_transfer:
+                        bank_to_transfer = (ip_to_transfer, port_to_transfer)
+                        # Redireciona para a função de transferência existente em transactions
+                        response = transfer(request, value_to_transfer, bank_to_transfer, client_to_transfer)
+                        return response
+                    else:
+                        messages.error(request, "Você precisa inserir o banco, a agência e o cliente.")
+                        return redirect('transaction_page')
+                    
                 elif action == 'deposit':
-                    pass
+                    value_to_transfer = form.cleaned_data['value_to_transfer']
+                    user.balance += value_to_transfer
+                    user.save()
+                    return redirect('my_account_page')
 
                 elif action == 'payment':
                     pass
